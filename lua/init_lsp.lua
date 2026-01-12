608948fbd0fb101d2b808653df524470dce14e4a
Markus Törnqvist <mjt@nysv.org>
2026-01-12 17:11:40 +0000

    Drop omnisharp to test pure csharp

--- lua/init_lsp.lua
+++ lua/init_lsp.lua
@@ -174,8 +174,6 @@ function setup_servers()
 
   -- csharp // `dotnet tool install --global csharp-ls`
   local csharp_ls_bin = vim.fn.join({vim.fn.expand('$HOME'), '.dotnet', 'tools', 'csharp-ls'}, '/')
-  -- XXX: but using that `--languageserver` approach just fails autocompletes and docs
-  local omnisharp_bin = vim.fn.join({vim.fn.expand('$HOME'), '.cache', 'omnisharp-vim', 'omnisharp-roslyn', 'OmniSharp.exe'}, '/')
 
   if vim.fn.executable('ruff-lsp') == 1 then
     lspconfig.ruff_lsp.setup {
@@ -241,56 +239,4 @@ function setup_servers()
     on_attach = on_attach,
   }
 
-  lspconfig.omnisharp.setup {
-    -- cmd = { '/bin/mono', omnisharp_bin, '--languageserver' , '--hostPID', tostring(pid) },
-    -- cmd = { "dotnet", "/home/mjt/.cache/omnisharp-vim/omnisharp-roslyn/OmniSharp.dll" },
-
-    ---- Not needed, these are added by lspconfig
-    -- cmd = { omnisharp_bin, '--languageserver', '--hostPID', tostring(pid) };
-
-    cmd = { omnisharp_bin };
-
-    -- https://github.com/Hoffs/omnisharp-extended-lsp.nvim
-    handlers = {
-      ["textDocument/definition"] = require('omnisharp_extended').handler,
-    },
-
-    -- Enables support for reading code style, naming convention and analyzer
-    -- settings from .editorconfig.
-    enable_editorconfig_support = true,
-
-    -- If true, MSBuild project system will only load projects for files that
-    -- were opened in the editor. This setting is useful for big C# codebases
-    -- and allows for faster initialization of code navigation features only
-    -- for projects that are relevant to code that is being edited. With this
-    -- setting enabled OmniSharp may load fewer projects and may thus display
-    -- incomplete reference lists for symbols.
-    -- enable_ms_build_load_projects_on_demand = true,
-    enable_ms_build_load_projects_on_demand = false,
-
-    -- Enables support for roslyn analyzers, code fixes and rulesets.
-    enable_roslyn_analyzers = false,
-
-    -- Specifies whether 'using' directives should be grouped and sorted during
-    -- document formatting.
-    organize_imports_on_format = false,
-
-    -- Enables support for showing unimported types and unimported extension
-    -- methods in completion lists. When committed, the appropriate using
-    -- directive will be added at the top of the current file. This option can
-    -- have a negative impact on initial completion responsiveness,
-    -- particularly for the first few completion sessions after opening a
-    -- solution.
-    enable_import_completion = false,
-
-    -- Specifies whether to include preview versions of the .NET SDK when
-    -- determining which version to use for project loading.
-    sdk_include_prereleases = false,
-
-    -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-    -- true
-    analyze_open_documents_only = false,
-
-    on_attach = on_attach,
-  }
 end
