2272dbd33fe00e752465e2d2b2d3277f1e7458bb
Markus Törnqvist <mjt@nysv.org>
2026-04-01 09:25:11 +0300

    Replace nvim-treesitter-refactor with -locals
    Though empty for 5 months(!)

--- lua/init_treesitter.lua
+++ lua/init_treesitter.lua
@@ -11,27 +11,6 @@ require('nvim-treesitter.configs').setup {
   textobjects = { enable = true },
   indent = { enable = true },
   fold = { enable = true },
-  refactor = {
-    highlight_definitions = { enable = false },
-    highlight_current_scope = { enable = false },
-    smart_rename = {
-      enable = true,
-      disable = { "c_sharp" },
-      keymaps = {
-        smart_rename = "grr",
-      },
-      navigation = {
-        enable = true,
-        keymaps = {
-          goto_definition = "gnd",
-          list_definitions = "gnD",
-          list_definitions_toc = "gO",
-          goto_next_usage = "<a-*>",
-          goto_previous_usage = "<a-#>",
-        },
-      },
-    },
-  },
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
